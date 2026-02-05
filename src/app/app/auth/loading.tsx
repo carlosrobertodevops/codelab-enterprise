22dad7004e20ffe2e1cc42a511c6332e4b529fe5
“geniuscodemycombr” <geniuscodemy.com.br@gmail.com>
2026-02-05 16:11:54 -0300

    Ajustes com Looding e spinner.

---
+++ src/app/app/auth/loading.tsx
@@ -0,0 +1,7 @@
+import LoadingSkeleton from './components/ui/skeleton.tsx';
+
+
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return <LoadingSkeleton />;
+}
